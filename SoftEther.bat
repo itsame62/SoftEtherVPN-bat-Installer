85522a0e92e92c7aed412272c274cf108f7a83e8
itsame62 <combinasionlagrande720@gmail.com>
2025-10-08 15:43:07 +0700

    Update SoftEther.bat

--- SoftEther.bat
+++ SoftEther.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFAtBWBaLAE+1EbsQ5+n//NaUrUsJUfY3fZ2WzKSAbukQ5SU=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFAtBWBaLAE+1EbsQ5+n//Na0rUsJcfY3fZ2b87qdNOEf5kD2O5M10xo=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 
 @echo off
 TITLE SoftEther VPN Installer
@@ -60,4 +27,5 @@ GOTO End
 
 :End
 timeout /t 1 >nul
+
 exit
